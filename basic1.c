85920fd449d222724f4efd7bb98b93893acc8e03
rohit226 <57681273+rohit226@users.noreply.github.com>
2022-03-16 10:26:27 +0530

    C files

---
+++ basic1.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+int main()
+{
+    //unsigned char ch = 1500;//
+    //char ch = 1200;//
+
+    //short int num = 90000;//
+
+    //long long int num;//
+    char ch;
+    printf("%c",sizeof(ch));
+    //printf("%d",num);//
+     return 0;
+}
